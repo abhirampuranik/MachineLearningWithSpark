ccf11f210a5e65aae0110f460960bec2d63f6069
abhirampuranik <abhiramkpuranik@gmail.com>
2021-12-04 14:12:14 +0530

    preprocessed hashvectorized

--- client_Vect.py
+++ client_Vect.py
@@ -1,33 +1,29 @@
 import time
+import string
+import pickle
+import json
+import numpy as np
+import nltk
 from pyspark import SparkContext
-from pyspark.sql.functions import udf,variance
+from pyspark.sql.functions import udf
 from pyspark.streaming import StreamingContext
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
-import numpy as np
-import nltk
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.model_selection import train_test_split
 from pyspark.sql.functions import when
-import string
-import pickle
 from pyspark.sql.functions import udf
-from pyspark.sql.types import ArrayType, StringType
-
+from pyspark.sql.types import ArrayType, StringType, StructField, StructType
 
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-
-from sklearn.feature_extraction.text import TfidfVectorizer, HashingVectorizer
+from sklearn.feature_extraction.text import HashingVectorizer
 
 
 sc = SparkContext("local[2]", "spam")
 ssc = StreamingContext(sc,1)
 spark = SparkSession(sc)
-
 data = ssc.socketTextStream("localhost",6100)
 
-# nltk.download('stopwords')
+# nltk.download('stopwords') -- use once
 
 def process_text(text):
 	from nltk.corpus import stopwords
@@ -42,13 +38,29 @@ def send_subject(x):
 def send_spam_ham(x):
 	return x
 
+
 udf1 = udf(lambda x: process_text(x), ArrayType(StringType()))
 clf = MultinomialNB()
 def plswork(rdd):
+	
+	global clf
+	
+
 	from nltk.corpus import stopwords
-	if not rdd.isEmpty():
-		df = spark.read.json(rdd)
-		
+	if not rdd.isEmpty():		
+
+		Stream=rdd.collect()
+		vals=[i for val in Stream for i in list(json.loads(val).values())]
+
+		schema=StructType([
+			StructField('Subject',StringType(),False),
+			StructField('Message',StringType(),False),
+			StructField('Spam/Ham',StringType(),False)
+		])
+		df=spark.createDataFrame((Row(**d) for d in vals),schema)
+		df=df['Message','Spam/Ham', 'Subject']
+
+
 		n_df = df.select(udf1('Message').alias('udf1(Message)'), send_subject('Subject'), send_spam_ham('Spam/Ham'))
 		df1 = n_df.withColumnRenamed("Spam/Ham","SpamHam").withColumnRenamed("udf1(Message)","Message")
 		dfl = df1.withColumn("SpamHam", when(df1.SpamHam == "spam","1").when(df1.SpamHam == "ham","0").otherwise(df1.SpamHam))
@@ -63,7 +75,7 @@ def plswork(rdd):
 			target.append(' '.join(row[0]))
 		
 		y = np.array(target)
-		# tfidfconverter = TfidfVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
+		
 		vectorizer = HashingVectorizer(n_features=2**12,norm = 'l1',stop_words='english', alternate_sign=False)
 		X = vectorizer.fit_transform(messages).toarray()
 		X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
@@ -72,9 +84,10 @@ def plswork(rdd):
 		
 		print('Accuracy: ')
 		print(clf.score(X_test, y_test))
+		
 
 		with open('spam_classifier_MNB', 'wb') as picklefile:
-			print("done")
+			# print("done")
 			pickle.dump(clf,picklefile)
 
 
