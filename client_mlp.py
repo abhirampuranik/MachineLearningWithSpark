3b10e56487a6ee9ff23ecb702d0ba311b4e278ba
shashnavad <60545088+shashnavad@users.noreply.github.com>
2021-12-04 18:08:52 +0530

    Updated MLP using hashing vectorizer

--- client_mlp.py
+++ client_mlp.py
@@ -6,21 +6,18 @@ from pyspark.sql import SparkSession
 from pyspark.sql import Row
 import numpy as np
 import nltk
+import json
 from sklearn.neural_network import MLPClassifier
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from pyspark.sql.functions import when
 import string
 import pickle
 from pyspark.sql.functions import udf
 from pyspark.sql.types import ArrayType, StringType
-
-
 from sklearn.model_selection import train_test_split
+from pyspark.sql.types import ArrayType, StringType, StructField, StructType
+from sklearn.feature_extraction.text import HashingVectorizer
 
-
-from sklearn.feature_extraction.text import TfidfVectorizer
-tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range=(1, 2), stop_words='english')
 
 sc = SparkContext("local[2]", "spam")
 ssc = StreamingContext(sc,1)
@@ -49,10 +46,21 @@ def send_spam_ham(x):
 udf1 = udf(lambda x: process_text(x), ArrayType(StringType()))
 clf = MLPClassifier(random_state=1, max_iter = 5)
 def plswork(rdd):
+	global clf
 	from nltk.corpus import stopwords
 	if not rdd.isEmpty():
-		df = spark.read.json(rdd)
-		
+		Stream=rdd.collect()
+		vals=[i for val in Stream for i in list(json.loads(val).values())]
+
+		schema=StructType([
+			StructField('Subject',StringType(),False),
+			StructField('Message',StringType(),False),
+			StructField('Spam/Ham',StringType(),False)
+		])
+		df=spark.createDataFrame((Row(**d) for d in vals),schema)
+		df=df['Message','Spam/Ham', 'Subject']
+
+
 		n_df = df.select(udf1('Message').alias('udf1(Message)'), send_subject('Subject'), send_spam_ham('Spam/Ham'))
 		df1 = n_df.withColumnRenamed("Spam/Ham","SpamHam").withColumnRenamed("udf1(Message)","Message")
 		dfl = df1.withColumn("SpamHam", when(df1.SpamHam == "spam","1").when(df1.SpamHam == "ham","0").otherwise(df1.SpamHam))
@@ -67,8 +75,8 @@ def plswork(rdd):
 			target.append(' '.join(row[0]))
 		
 		y = np.array(target)
-		tfidfconverter = TfidfVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
-		X = tfidfconverter.fit_transform(messages).toarray()
+		vectorizer = HashingVectorizer(n_features=2**12,norm = 'l1',stop_words='english', alternate_sign=False)
+		X = vectorizer.fit_transform(messages).toarray()
 		X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 		clf.partial_fit(X_train, y_train, classes = np.unique(y_train))
@@ -82,7 +90,6 @@ def plswork(rdd):
 
 
 data.foreachRDD(lambda rdd: plswork(rdd))
-
 
 
 ssc.start()
